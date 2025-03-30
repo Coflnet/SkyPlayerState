6843fda689b1e266fc81bb0413517c1ce2e94c36
Äkwav <ekwav@coflnet.com>
2025-03-30 13:57:22 +0200

    make chest batch nullable

--- Models/UpdateMessage.cs
+++ Models/UpdateMessage.cs
@@ -13,9 +13,9 @@ public class UpdateMessage
     [Key(1)]
     public DateTime ReceivedAt;
     [Key(2)]
-    public ChestView Chest;
+    public ChestView? Chest;
     [Key(3)]
-    public List<string> ChatBatch;
+    public List<string>? ChatBatch;
     [Key(4)]
     public string PlayerId;
     [Key(5)]
