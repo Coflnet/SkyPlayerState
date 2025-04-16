1cd988fb587f341788cd3eab4e1bb97a6d22fe21
Äkwav <ekwav@coflnet.com>
2025-04-16 10:46:00 +0200

    log paid chest content

--- Services/Listener/KuudraListener.cs
+++ Services/Listener/KuudraListener.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Coflnet.Sky.PlayerState.Services;
 
@@ -8,8 +9,17 @@ public class KuudraListener : UpdateListener
     /// <inheritdoc/>
     public override async Task Process(UpdateArgs args)
     {
-        if(args.msg.Kind == Models.UpdateMessage.UpdateKind.Scoreboard)
+        if (args.msg.Kind == Models.UpdateMessage.UpdateKind.Scoreboard)
             await CheckKuudra(args);
+        if(args.msg.Kind == Models.UpdateMessage.UpdateKind.INVENTORY)
+            await GotInventory(args);
+    }
+
+    private async Task GotInventory(UpdateArgs args)
+    {
+        if (args.msg.Chest?.Name != "Paid Chest")
+            return;
+        Console.WriteLine($"Got paid chest {args.msg.PlayerId} {JsonConvert.SerializeObject(args.msg.Chest)}");
     }
 
     private async Task CheckKuudra(UpdateArgs args)
