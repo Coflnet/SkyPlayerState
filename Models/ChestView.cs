444cccd8abad4e046c0f3b62b86d991bd696c89d
Äkwav <ekwav@coflnet.com>
2025-07-20 00:03:10 +0200

    add block position

--- Models/ChestView.cs
+++ Models/ChestView.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Coflnet.Sky.Core;
 using MessagePack;
 
 namespace Coflnet.Sky.PlayerState.Models;
@@ -10,11 +11,16 @@ public class ChestView
     /// All items in the ui view
     /// </summary>
     [Key(0)]
-    public List<Item> Items = new ();
+    public List<Item> Items = new();
     /// <summary>
     /// Name of the chest
     /// </summary>
     [Key(1)]
     public string Name = string.Empty;
+    /// <summary>
+    /// Position of the chest in the world
+    /// </summary>
+    [Key(2)]
+    public BlockPos? Position;
 }
 #nullable restore
